3f9b53480112d48ae018dc639cbfc91cc5268617
waitwalker <waitwalker@163.com>
2021-01-05 14:27:45 +0800

    移除一些信息

--- Example/DeviceHelper/WWViewController.m
+++ Example/DeviceHelper/WWViewController.m
@@ -38,7 +38,6 @@
     
     NSLog(@"设备内网ip:%@", [DeviceManager sharedManager].intranetIPAddress);
     NSLog(@"设备外网ip:%@", [DeviceManager sharedManager].externalIPAddress);
-    NSLog(@"设备WiFi名称:%@", [DeviceManager sharedManager].WiFiName);
     
     NSLog(@"设备总存储空间:%ld", [DeviceManager sharedManager].diskTotalSize);
     NSLog(@"设备可用存储空间:%ld", [DeviceManager sharedManager].diskFreeSize);
@@ -46,6 +45,24 @@
     NSLog(@"设备总内存空间:%ld", [DeviceManager sharedManager].memoryTotalSize);
     NSLog(@"设备可用内存空间:%ld", [DeviceManager sharedManager].memoryFreeSize);
     NSLog(@"设备是否代理:%d", [DeviceManager sharedManager].isProxy);
+    
+    
+    #warning 该方法是私有API，上线会被拒
+    // 由于每款手机同一颜色具体色号不一样，如 iPhone 6和 iPhone SE粉色的两个具体色值是不一样的，因此在这里只能返回一个色号
+    UIDevice *device = [UIDevice currentDevice];
+    SEL selector = NSSelectorFromString(@"deviceInfoForKey:");
+    if (![device respondsToSelector:selector]) {
+        selector = NSSelectorFromString(@"_deviceInfoForKey:");
+    }
+    if ([device respondsToSelector:selector]) {
+        // 消除警告“performSelector may cause a leak because its selector is unknown”
+        IMP imp = [device methodForSelector:selector];
+        NSString * (*func)(id, SEL, NSString *) = (void *)imp;
+    
+        NSString *deviceColor  = func(device, selector, @"DeviceColor");
+        NSString *deviceEnclosureColor  = func(device, selector, @"DeviceEnclosureColor");
+        NSLog(@"deviceColor --> %@  deviceEnclosureColor --> %@ ", deviceColor, deviceEnclosureColor);
+    }
     
 }
 
